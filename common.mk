9c77f8e0c530f13179bfb1c6e2b74603c09ac6f8
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-11 17:27:24 +0100

    sm8450-common: Import powerhint.json from lahaina
    from https://github.com/PixelOS-Devices/device_oneplus_sm8350-common/blob/fourteen-qpr1/configs/powerhint_lahaina.json
    
    Change-Id: I2ddb226cfd379b6b287a4393058c12dd579d38de

--- common.mk
+++ common.mk
@@ -381,6 +381,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # PowerShare
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.xiaomi
